655a845c962a83c01d7b1c4a4e0b1cbe0a2e3603
nck06 <5410928+nck06@users.noreply.github.com>
2018-11-11 07:29:19 +0000

    first commit

---
+++ src/components/Sample.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="sample">
+    <h1>{{ msg }}</h1>
+    <p>this msg length is {{ msgLength }}</p>
+    <input type="text" v-model="newMsg" />
+    <button @click="update">更新</button>
+    <button @click="repeat">繰り返す</button>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import * as types from '@/store/mutation-types';
+export default {
+  name: 'Sample',
+  data() {
+    return {
+    //   msg: 'Welcome to My Vuex Sample',
+        newMsg: null
+    };
+  },
+  computed: {
+    // msg() {
+    //   return this.$store.state.msg;
+    // },
+    msgLength() {
+        return this.msg.length;
+    },
+    ...mapState({
+        msg: state => state.msg,
+    })
+  },
+  methods: {
+    update() {
+    //   this.$store.state.msg = this.newMsg;
+        this.$store.commit(types.UPDATE_MESSAGE, this.newMsg);
+    },
+    repeat() {
+        this.$store.dispatch('repeat');
+    },
+  }
+};
+</script>
